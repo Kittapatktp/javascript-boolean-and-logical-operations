d6aad84ad208d119e2a045102acbe4f426b94766
Your NamePEem <you@example.comkittapat.ktp@gmail.com>
2026-03-08 21:41:49 +0700

    clear(assignment) : chapter 3)

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
-let isOver18;
+let isOver18 ;
 let hasCriminalBlacklist;
 
 let isAllow;
 
 // Start coding here
+// Jame
+isOver18 = false
+hasCriminalBlacklist = false
+isAllow = isOver18 && !hasCriminalBlacklist
+
 
 console.log(isAllow);
